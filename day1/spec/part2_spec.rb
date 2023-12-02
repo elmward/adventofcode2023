dc713ceff6b1ab2c85f9f43df41d3df8b680939b
Mark Ashton <meashton@gmail.com>
2023-12-02 06:20:54 -0600

    A little prepatory cleanup before making day1 part 2 iterative

--- day1/spec/part2_spec.rb
+++ day1/spec/part2_spec.rb
@@ -1,22 +1,22 @@
 require_relative "../part2.rb"
 
-describe "left_number" do
+describe "first_number" do
   it "identifies a digit" do
-    expect(left_number("1")).to eq "1"
+    expect(first_number("1")).to eq "1"
   end
 
   %w(one two three four five six seven eight nine).each.with_index do |word, i|
     it "identifies #{word}" do
-      expect(left_number(word)).to eq (i + 1).to_s
+      expect(first_number(word)).to eq (i + 1).to_s
     end
   end
 
   it "prefers the leftmost word" do
-    expect(left_number("twone")).to eq "2"
+    expect(first_number("twone")).to eq "2"
   end
 
   it "finds the leftmost number even if internal" do
-    expect(left_number("abcninedef1")).to eq "9"
+    expect(first_number("abcninedef1")).to eq "9"
   end
 end
 
